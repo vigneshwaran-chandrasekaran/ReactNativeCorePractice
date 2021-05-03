e2bb678e102fe400dc5f25c65e9f4070f86d5b7d
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-05-03 23:00:50 +0530

    styled component TextInput added

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {StatusBar, Text} from 'react-native';
 import styled from 'styled-components';
 
-const Btn = styled.Button`
-  background-color: green;
-  color: yellow;
-  font-size: 10px;
+const AppBtn = styled.TouchableOpacity`
+  padding: 10px;
+  border-radius: 10px;
+  background-color: ${props => props.backgroundColor ?? 'blue'};
 `;
 
-const MyBtn = styled.TouchableOpacity`
-  background-color: orange;
-  color: yellow;
-  font-size: 10px;
+const ButtonText = styled.Text`
+  font-size: 15px;
+  color: ${props => props.textColor};
   text-align: center;
-  padding: 10px;
 `;
+
+const CustomButton = props => (
+  <ButtonContainer
+    onPress={() => alert('Hi!')}
+    backgroundColor={props.backgroundColor}>
+    <ButtonText textColor={props.textColor}>{props.text}</ButtonText>
+  </ButtonContainer>
+);
 
 const Title = styled.Text`
   background-color: red;
@@ -24,38 +30,65 @@ const Title = styled.Text`
   padding: 20px;
 `;
 
+const AppTextInput = styled.TextInput`
+  padding: 5px 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  font-size: 16px;
+  color: black;
+  border-radius: 5px;
+`;
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  padding: 20px;
+`;
+
 export default function App() {
+  const [text, onChangeText] = React.useState(null);
+  const [number, onChangeNumber] = React.useState(null);
+
   function handleBtnClick() {
     alert('Button clicked wow');
   }
 
   return (
-    <View>
+    <Container style={{marginTop: StatusBar.currentHeight || 0}}>
+      {/* <Text>Test</Text>
       <Text>Test</Text>
       <Text>Test</Text>
       <Text>Test</Text>
       <Text>Test</Text>
-      <Text>Test</Text>
-
       <Btn onPress={handleBtnClick} title="Learn More" />
-
-      <MyBtn onPress={handleBtnClick}>
-        <Text>Wow</Text>
-      </MyBtn>
-
       <Button
         onPress={handleBtnClick}
         title="Learn More"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
-
       <Button
         onPress={handleBtnClick}
         title="Learn More"
         accessibilityLabel="Learn more about this purple button"
       />
       <Title>Vigneshwaran</Title>
-    </View>
+      */}
+      <AppTextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Username"
+      />
+      <AppTextInput
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Mobile"
+        keyboardType="numeric"
+      />
+      <AppBtn onPress={handleBtnClick}>
+        <Text>Submit</Text>
+      </AppBtn>
+    </Container>
   );
 }
