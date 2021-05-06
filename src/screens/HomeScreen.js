622781f3a9b3f108165d39f85f622a36723a643e
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-05-06 10:25:09 +0530

    react-native-dropdown-picker component tested

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
 import {Picker} from '@react-native-picker/picker';
+/**
+ * This Picker component is ok component only, don't have any fancy details,
+ * 2/5 rating
+ */
 import React, {useState} from 'react';
 import {Text, View} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const HomeScreen = () => {
 	const [selectedLanguage, setSelectedLanguage] = useState('js');
+	const [open, setOpen] = useState(false);
+	const [value, setValue] = useState(null);
+	const [items, setItems] = useState([
+		{label: 'Apple', value: 'apple', disabled: 'disabled'},
+		{label: 'Banana', value: 'banana'},
+		{label: 'one', value: 'one'},
+		{label: 'two', value: 'two'},
+		{label: 'three', value: 'three'},
+		{label: 'four', value: 'four'},
+		{label: 'five', value: 'five'},
+	]);
 
 	return (
 		<View style={{flex: 1}}>
@@ -22,7 +38,7 @@ const HomeScreen = () => {
 					style={{
 						backgroundColor: 'pink',
 						color: 'black',
-						fontSize: '18px',
+						fontSize: 18,
 					}}
 					prompt="Please select the language"
 					selectedValue={selectedLanguage}
@@ -38,6 +54,40 @@ const HomeScreen = () => {
 					<Picker.Item label="Java" value="java" />
 					<Picker.Item label="JavaScript" value="js" />
 				</Picker>
+				<DropDownPicker
+					// searchable={false}
+					open={open}
+					value={value}
+					items={items}
+					setValue={setValue}
+					setItems={setItems}
+					setOpen={setOpen}
+					multiple={true}
+					min={0}
+					max={3}
+					style={
+						{
+							// backgroundColor: 'crimson',
+						}
+					}
+					containerStyle={{
+						backgroundColor: 'pink',
+						borderColor: 'red',
+					}}
+					textStyle={{
+						fontSize: 15,
+					}}
+					labelStyle={{
+						fontWeight: 'bold',
+					}}
+					placeholder="Select an item, to choose"
+					placeholderStyle={{
+						color: 'red',
+						fontWeight: 'bold',
+						fontSize: 20,
+					}}
+					mode="BADGE"
+				/>
 			</View>
 		</View>
 	);
