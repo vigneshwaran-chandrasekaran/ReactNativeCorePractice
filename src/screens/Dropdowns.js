444ee9f4978ef8ffa732ce2a3881e118f1e12493
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-05-09 14:11:38 +0530

    react-native-picker-select dropdown component added and tested

--- src/screens/Dropdowns.js
+++ src/screens/Dropdowns.js
@@ -4,18 +4,46 @@ import {Picker} from '@react-native-picker/picker';
  * 2/5 rating
  */
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 /**
  * This ModalDropdown component is ok component only, don't have any fancy details, it below Picker component
  * 1.75/5 rating
  */
 import ModalDropdown from 'react-native-modal-dropdown';
+import RNPickerSelect from 'react-native-picker-select';
+// https://snack.expo.io/@lfkwtz/react-native-picker-select
+
+const pickerSelectStyles = StyleSheet.create({
+	inputIOS: {
+		fontSize: 16,
+		paddingVertical: 12,
+		paddingHorizontal: 10,
+		borderWidth: 1,
+		borderColor: 'gray',
+		borderRadius: 4,
+		color: 'black',
+		paddingRight: 30, // to ensure the text is never behind the icon
+	},
+	inputAndroid: {
+		margin: 5,
+		fontSize: 16,
+		paddingHorizontal: 10,
+		paddingVertical: 8,
+		borderWidth: 1,
+		borderColor: 'purple',
+		borderRadius: 8,
+		color: 'red',
+		paddingRight: 30, // to ensure the text is never behind the icon
+	},
+});
 
 const Dropdowns = () => {
 	const [selectedLanguage, setSelectedLanguage] = useState('js');
 	const [open, setOpen] = useState(false);
 	const [value, setValue] = useState(null);
+	const [language, setLanguage] = useState('');
+
 	const [items, setItems] = useState([
 		{label: 'Apple', value: 'apple', disabled: 'disabled'},
 		{label: 'Banana', value: 'banana'},
@@ -104,6 +132,30 @@ const Dropdowns = () => {
 					fontSize: 20,
 				}}
 				mode="BADGE"
+			/>
+
+			<RNPickerSelect
+				placeholder={{
+					label: 'Select you favourite language',
+					value: 'TypeStript',
+					color: '#9EA0A4',
+				}}
+				useNativeAndroidPickerStyle={false}
+				onValueChange={res => {
+					console.log('res res res', res);
+					setLanguage(res);
+				}}
+				value={language}
+				items={[
+					{label: 'JavaScript', value: 'JavaScript'},
+					{label: 'TypeStript', value: 'TypeStript'},
+					{label: 'Python', value: 'Python'},
+					{label: 'Java', value: 'Java'},
+					{label: 'C++', value: 'C++'},
+					{label: 'C', value: 'C'},
+				]}
+				textInputProps={{underlineColor: 'yellow'}}
+				style={pickerSelectStyles}
 			/>
 		</View>
 	);
