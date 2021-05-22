c6c910b55a6021af08e7048088b0f110e5c73287
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-05-22 13:35:12 +0530

    custom drawer style added

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+	createDrawerNavigator,
+	DrawerContentScrollView,
+	DrawerItem,
+	DrawerItemList,
+} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {SampleForm} from 'components';
 import React, {useState} from 'react';
-import {StatusBar, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, View} from 'react-native';
 import 'react-native-gesture-handler';
+import {
+	Avatar,
+	Caption,
+	Drawer,
+	Paragraph,
+	Switch,
+	Text,
+	Title,
+	TouchableRipple,
+	useTheme,
+} from 'react-native-paper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {ContactScreen, HomeScreen} from 'screens';
 import styled from 'styled-components';
@@ -17,25 +33,179 @@ const Container = styled.View`
 `;
 
 const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+const DrawerSection = createDrawerNavigator();
 
 const STYLES = ['default', 'dark-content', 'light-content'];
 const TRANSITIONS = ['fade', 'slide', 'none'];
 
-//
-const DrawerContent = props => {
+export function DrawerContent(props) {
+	const paperTheme = useTheme();
+
+	function signOut() {
+		alert('signOut');
+	}
+
 	return (
-		<View styke={{flex: 1}}>
-			<Text>Hello data</Text>
-			<Text>Hello 123</Text>
-			{/* <DrawerContentScrollView {...props}>
-			</DrawerContentScrollView> */}
-			{/* <Drawer.Section>
-				<DrawerItem label="hello" />
-			</Drawer.Section> */}
+		<View style={{flex: 1}}>
+			<DrawerContentScrollView {...props}>
+				<DrawerItemList {...props} />
+				<View style={styles.drawerContent}>
+					<DrawerItemList {...props} />
+					<View style={styles.userInfoSection}>
+						<View style={{flexDirection: 'row', marginTop: 15}}>
+							<Avatar.Image
+								source={{
+									uri:
+										'https://api.adorable.io/avatars/50/abott@adorable.png',
+								}}
+								size={50}
+							/>
+							<View
+								style={{
+									marginLeft: 15,
+									flexDirection: 'column',
+								}}>
+								<Title style={styles.title}>John Doe</Title>
+								<Caption style={styles.caption}>@j_doe</Caption>
+							</View>
+						</View>
+
+						<View style={styles.row}>
+							<View style={styles.section}>
+								<Paragraph
+									style={[styles.paragraph, styles.caption]}>
+									80
+								</Paragraph>
+								<Caption style={styles.caption}>
+									Following
+								</Caption>
+							</View>
+							<View style={styles.section}>
+								<Paragraph
+									style={[styles.paragraph, styles.caption]}>
+									100
+								</Paragraph>
+								<Caption style={styles.caption}>
+									Followers
+								</Caption>
+							</View>
+						</View>
+					</View>
+
+					<View
+						style={{
+							flexDirection: 'row',
+							alignItems: 'center',
+							marginTop: 15,
+						}}>
+						<Ionicons
+							name="home-outline"
+							size={24}
+							style={{paddingLeft: 20}}
+						/>
+						<Text style={{fontSize: 18, paddingLeft: 20}}>Two</Text>
+					</View>
+
+					<Drawer.Section style={styles.drawerSection}>
+						<DrawerItem
+							icon={({color, size}) => (
+								<Ionicons
+									name="home-outline"
+									color={color}
+									size={size}
+								/>
+							)}
+							label="Home"
+							onPress={() => {
+								props.navigation.navigate('Home');
+							}}
+						/>
+						<DrawerItem
+							icon={({color, size}) => (
+								<Ionicons
+									name="account-outline"
+									color={color}
+									size={size}
+								/>
+							)}
+							label="Home"
+							onPress={() => {
+								props.navigation.navigate('Home');
+							}}
+						/>
+						<DrawerItem
+							icon={({color, size}) => (
+								<Ionicons
+									name="bookmark-outline"
+									color={color}
+									size={size}
+								/>
+							)}
+							label="Home"
+							onPress={() => {
+								props.navigation.navigate('Home');
+							}}
+						/>
+						<DrawerItem
+							icon={({color, size}) => (
+								<Ionicons
+									name="settings-outline"
+									color={color}
+									size={size}
+								/>
+							)}
+							label="Home"
+							onPress={() => {
+								props.navigation.navigate('Home');
+							}}
+						/>
+						<DrawerItem
+							icon={({color, size}) => (
+								<Ionicons
+									name="account-check-outline"
+									color={color}
+									size={size}
+								/>
+							)}
+							label="Home"
+							onPress={() => {
+								props.navigation.navigate('Home');
+							}}
+						/>
+					</Drawer.Section>
+					<Drawer.Section title="Preferences">
+						<TouchableRipple
+							onPress={() => {
+								signOut();
+							}}>
+							<View style={styles.preference}>
+								<Text>Dark Theme</Text>
+								<View pointerEvents="none">
+									<Switch value={paperTheme.dark} />
+								</View>
+							</View>
+						</TouchableRipple>
+					</Drawer.Section>
+				</View>
+			</DrawerContentScrollView>
+			<Drawer.Section style={styles.bottomDrawerSection}>
+				<DrawerItem
+					icon={({color, size}) => (
+						<Ionicons
+							name="exit-to-app"
+							color={color}
+							size={size}
+						/>
+					)}
+					label="Sign Out"
+					onPress={() => {
+						signOut();
+					}}
+				/>
+			</Drawer.Section>
 		</View>
 	);
-};
+}
 
 const MainTabScreen = () => (
 	<Tab.Navigator
@@ -95,15 +265,61 @@ export default function App() {
 					hidden={hidden}
 				/>
 
-				<Drawer.Navigator
+				<DrawerSection.Navigator
 					drawerContent={props => <DrawerContent {...props} />}>
-					<Drawer.Screen
+					<DrawerSection.Screen
 						name="HomeDrawer"
 						component={MainTabScreen}
 					/>
-					<Drawer.Screen name="Form" component={SampleForm} />
-				</Drawer.Navigator>
+					<DrawerSection.Screen name="Form" component={SampleForm} />
+				</DrawerSection.Navigator>
 			</Container>
 		</NavigationContainer>
 	);
 }
+
+const styles = StyleSheet.create({
+	drawerContent: {
+		flex: 1,
+	},
+	userInfoSection: {
+		paddingLeft: 20,
+	},
+	title: {
+		fontSize: 16,
+		marginTop: 3,
+		fontWeight: 'bold',
+	},
+	caption: {
+		fontSize: 14,
+		lineHeight: 14,
+	},
+	row: {
+		marginTop: 20,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	section: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginRight: 15,
+	},
+	paragraph: {
+		fontWeight: 'bold',
+		marginRight: 3,
+	},
+	drawerSection: {
+		marginTop: 15,
+	},
+	bottomDrawerSection: {
+		marginBottom: 15,
+		borderTopColor: '#f4f4f4',
+		borderTopWidth: 1,
+	},
+	preference: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingVertical: 12,
+		paddingHorizontal: 16,
+	},
+});
