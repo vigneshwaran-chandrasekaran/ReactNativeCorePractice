33e2f41abba18af55caa86010f7694073510c490
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-05-27 21:45:11 +0530

    axios api call added

--- src/screens/ApiCallScreen.js
+++ src/screens/ApiCallScreen.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, Text, View} from 'react-native';
+import styled from 'styled-components';
+
+const Card = styled.View`
+	margin: 10px;
+	padding: 10px;
+	background-color: white;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+`;
+
+const Name = styled.Text`
+	font-weight: bold;
+`;
+
+const Email = styled.Text`
+	color: blue;
+`;
 
 const ApiCallScreen = () => {
+	const [persons, setPersons] = useState([]);
+
+	useEffect(() => {
+		axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+			const data = res.data;
+			setPersons(data);
+			console.log('persons', data);
+		});
+	}, []);
+
 	return (
 		<View>
-			<Text>ApiCallScreen</Text>
+			<ScrollView>
+				{persons.map((item, i) => (
+					<Card key={i}>
+						<Name>{item?.name}</Name>
+						<Text>{item?.username}</Text>
+						<Email>{item?.website}</Email>
+						<Text>{item?.email}</Text>
+					</Card>
+				))}
+			</ScrollView>
 		</View>
 	);
 };
